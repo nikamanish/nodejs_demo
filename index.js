d7fcf78ed7d6846f9c725a366eee1532dc948927
Idyllic <nikamanish007@gmail.com>
2017-01-18 14:20:56 +0530

    Make a Simple Calculator

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 var http = require('http');
-var mustache = require('mustache')
-var randomNumber = require('./random-number.js')
+//var mustache = require('mustache')
+//var randomNumber = require('./random-number.js')
+
+/*
+** Simple calculator.
+** Pass the arguments in url as follows
+** 			i => First Number
+**			j => Second Number
+**			op => Operator
+**					add - addition
+**					sub - subtract
+**					mul - multiply
+**					div - divide
+*/
+
 requestHandler = function(req, res){
 	console.log("in request handler");
-	if (req.url == '/') {
-		
-
-		result = randomNumber() + " "+ randomNumber() + " " + randomNumber();
-		console.log(result);
-		res.end(result + " ");
-
+	var url = require('url');
+	var query = url.parse(req.url, true).query;
+	var ans = null;
+	var n1 = parseFloat(query["i"]);
+	var n2 = parseFloat(query["j"]);
+	switch(query["op"])
+	{
+		case "add": ans = n1 + n2;break;
+		case "sub": ans = n1 - n2;break;
+		case "mul": ans = n1 * n2;break;
+		case "div": ans = n1 / n2;break;
 	}
-	else{
-		res.end("Dont be far from home");
-	}
+
+
+	res.end("" + ans);
+	//switch()
 }
 
 
 var server = http.createServer(requestHandler);
 
-server.listen(9100, function(){
+server.listen(8000, function(){
 	console.log("Started server on port 8000");
 })
